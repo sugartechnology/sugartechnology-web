585fd9fd0e9fb9385fc08ff1b6473f4746625579
sugartechnology <>
2023-04-08 02:22:09 +0300

    sugar customer comments index added

--- src/components/CustomerComments/CustomerComments.js
+++ src/components/CustomerComments/CustomerComments.js
@@ -1,225 +1,66 @@
 import './CustomerComments.css';
-import { useTranslation} from "react-i18next";
+import { useTranslation } from "react-i18next";
+import { useState } from 'react';
 
-export const CustomerComments = props =>{
-    const {t} = useTranslation();
+export const CustomerComments = props => {
+    const { t } = useTranslation();
+    const [index, setIndex] = useState(0);
+    const array = [{
+        trustedByMillionsOfCompany: "trustedByMillionsOfCompany",
+        commentSpan: "commentSpan",
+        commentHeader: "commentHeader",
+        commentAuthor: "Yusuf Fatih Erol"
+    },
 
-    function showComment1(){
-        var comment1 = document.getElementById("firstComment");
-        var btn1 = document.getElementById("comment1Button");
-        var comment2 = document.getElementById("secondCustomerComment");
-        var btn2 = document.getElementById("comment2Button");
-        var comment3 = document.getElementById("thirdCustomerComment");
-        var btn3 = document.getElementById("comment3Button");
-        if(comment1.style.display === "none"){
-            comment1.style.display = "flex";
-            btn1.style.background = "#ED401A";
-            comment2.style.display = "none";
-            btn2.style.background = "#CCCCCC";
-            comment3.style.display = "none";
-            btn3.style.background = "#CCCCCC";
-        }
-        else{
-            comment1.style.display = "none"
-        }
+    {
+        trustedByMillionsOfCompany: "trustedByMillionsOfCompany1",
+        commentSpan: "commentSpan1",
+        commentHeader: "commentHeader1",
+        commentAuthor: "Abdurrahman  Türkeri"
+    },
+    {
+        trustedByMillionsOfCompany: "trustedByMillionsOfCompany3",
+        commentSpan: "commentSpan3",
+        commentHeader: "commentHeader4",
+        commentAuthor: "Ali  Yalcin "
+    }];
+
+    const next = () => {
+        setIndex((index + 1) % array.length);
     }
-    function showComment2(){
-        var comment1 = document.getElementById("firstComment");
-        var btn1 = document.getElementById("comment1Button");
-        var comment2 = document.getElementById("secondCustomerComment");
-        var btn2 = document.getElementById("comment2Button");
-        var comment3 = document.getElementById("thirdCustomerComment");
-        var btn3 = document.getElementById("comment3Button");
-        if(comment2.style.display === "none"){
-            comment2.style.display = "flex";
-            btn2.style.background = "#ED401A";
-            comment1.style.display = "none";
-            btn1.style.background = "#CCCCCC";
-            comment3.style.display = "none";
-            btn3.style.background = "#CCCCCC";
-        }
-        else{
-            comment2.style.display = "none"
-        }
+
+    const prev = () => {
+        setIndex((index - 1) < 0 ? index - 1 + array.length : index - 1);
     }
-    function showComment3(){
-        var comment1 = document.getElementById("firstComment");
-        var btn1 = document.getElementById("comment1Button");
-        var comment2 = document.getElementById("secondCustomerComment");
-        var btn2 = document.getElementById("comment2Button");
-        var comment3 = document.getElementById("thirdCustomerComment");
-        var btn3 = document.getElementById("comment3Button");
-        if(comment3.style.display === "none"){
-            comment3.style.display = "flex";
-            btn3.style.background = "#ED401A";
-            comment2.style.display = "none";
-            btn2.style.background = "#CCCCCC";
-            comment1.style.display = "none";
-            btn1.style.background = "#CCCCCC";
-        }
-        else{
-            comment3.style.display = "none"
-        }
-    }
-    
-    return(
+
+    return (
         <div className='customerCommentsContainer'>
-            <img className='commentsLeftButton' alt='' src={'assets/img/commentsLeftButton.svg'}></img>
+            <img className='commentsLeftButton' alt='' src={'assets/img/commentsLeftButton.svg'} onClick={() => { prev(); }}></img>
             <div className='firstCommnetBackground'>
-                <img className='redBackgroundMobile' alt='' src={'./assets/img/customerCommentMobileBackground.svg'} style={{display: "none"}}></img>
+                <img className='redBackgroundMobile' alt='' src={'./assets/img/customerCommentMobileBackground.svg'} style={{ display: "none" }}></img>
                 <img className='redBackground' alt='' src={'assets/img/customerCommentRedBackground.svg'}></img>
-                    <div className='firstComment' id='firstComment'>
-                        <div className='commentSpans'>
-                            <a className='firstCommentSpan'>{t("trustedByMillionsOfCompany")}</a>
-                            <a className='secondCommentSpan'>“</a>
-                            <a className='thirdCommentSpan'>{t("firstCommentSpan")}</a>
-                        </div>
-                        <div className='commentCustomer'>
-                            <img className='customerPhoto' alt='' src={'assets/img/TepeHomePhoto.svg'}></img>
-                            <h1>{t("firstCommentHeader")}</h1>
-                            <a>{t("firstCommentAuthor")}</a>
-                            <img alt='' src={'assets/img/Star.svg'}></img>
-                        </div>
+                <div className='firstComment' id='firstComment'>
+                    <div className='commentSpans'>
+                        <a className='firstCommentSpan'>{t(array[index].trustedByMillionsOfCompany)}</a>
+                        <a className='secondCommentSpan'>“</a>
+                        <a className='thirdCommentSpan'>{t(array[index].commentSpan)}</a>
                     </div>
-                    <div className='secondCustomerComment' id='secondCustomerComment' style={{display: "none"}}>
-                        <div className='commentSpans'>
-                            <a className='firstCommentSpan'>{t("trustedByMillionsOfCompany")}</a>
-                            <a className='secondCommentSpan'>“</a>
-                            <a className='thirdCommentSpan'>{t("secondCommentSpan")}</a>
-                        </div>
-                        <div className='commentCustomer'>
-                            <img className='customerPhoto' alt='' src={'assets/img/TepeHomePhoto.svg'}></img>
-                            <h1>{t("secondCommentHeader")}</h1>
-                            <a>{t("secondCommentAuthor")}</a>
-                            <img alt='' src={'assets/img/Star.svg'}></img>
-                        </div>
+                    <div className='commentCustomer'>
+                        <img className='customerPhoto' alt='' src={'assets/img/TepeHomePhoto.svg'}></img>
+                        <h1>{t(array[index].commentHeader)}</h1>
+                        <a>{array[index].commentAuthor}</a>
+                        <img alt='' src={'assets/img/Star.svg'}></img>
                     </div>
-                    <div className='thirdCustomerComment' id='thirdCustomerComment' style={{display: "none"}}>
-                        <div className='commentSpans'>
-                            <a className='firstCommentSpan'>{t("trustedByMillionsOfCompany")}</a>
-                            <a className='secondCommentSpan'>“</a>
-                            <a className='thirdCommentSpan'>{t("thirdCommentSpan")}</a>
-                        </div>
-                        <div className='commentCustomer'>
-                            <img className='customerPhoto' alt='' src={'assets/img/TepeHomePhoto.svg'}></img>
-                            <h1>{t("thirdCommentHeader")}</h1>
-                            <a>{t("thirdCommentAuthor")}</a>
-                            <img alt='' src={'assets/img/Star.svg'}></img>
-                        </div>
-                    </div>
+                </div>
+
                 <div className='customerCommentsPageButtons'>
-                    <img onClick={showComment1} className='solution1Button' id='comment1Button'></img>
-                    <img onClick={showComment2} className='solution2Button' id='comment2Button'></img>
-                    <img onClick={showComment3} className='solution3Button' id='comment3Button'></img>
+                    <img onClick={() => { setIndex(0); }} className='solution1Button' id='comment1Button'></img>
+                    <img onClick={() => { setIndex(1); }} className='solution2Button' id='comment2Button'></img>
+                    <img onClick={() => { setIndex(2); }} className='solution3Button' id='comment3Button'></img>
                 </div>
             </div>
 
-            {/* <div className='secondComment'>
-                <div className='commentHeader'>
-                    <a>People love Sugar</a>
-                </div>
-                <div className='comments'>
-                    <div className='customerComment'>
-                        <div className='commentSymbol'>
-                            <img alt='' src={'assets/img/commentSymbol.svg'}></img>
-                        </div>
-                        <div className='commentSpan'>
-                            <a>Lorem ipsum dolor sit amet consectetur. Potenti turpis aenean tempus aliquam metus at. Ultricies vel adipiscing volutpat erat. Elit diam vulputate id ullamcorper in tellus vulputate donec. Faucibus quisque euismod sodales nunc. Velit mattis accumsan aenean dignissim fames nisl sed pulvinar. Nullam ut tortor in ac semper enim cursus quisque. Amet felis at consectetur faucibus. At nisl dictum in eu quisque eget. Integer lacinia non mattis diam ornare. Tempor leo erat risus faucibus nulla pharetra egestas ultrices.
-                            </a>
-                        </div>
-                        <div className='commentOwner'>
-                            <img alt='' src={'assets/img/TepeHomeLittlePhoto.svg'}></img>
-                            <a className='commentOwnerFirstSpan'>Lorem ipsum</a>
-                            <a className='commentOwnerSecondSpan'>Tepe home</a>
-                            <img alt='' src={'assets/img/littleStar.svg'}></img>
-                        </div>
-                    </div>
-                    <div className='customerComment'>
-                        <div className='commentSymbol'>
-                            <img alt='' src={'assets/img/commentSymbol.svg'}></img>
-                        </div>
-                        <div className='commentSpan'>
-                            <a>Lorem ipsum dolor sit amet consectetur. Potenti turpis aenean tempus aliquam metus at. Ultricies vel adipiscing volutpat erat. Elit diam vulputate id ullamcorper in tellus vulputate donec. Faucibus quisque euismod sodales nunc. Velit mattis accumsan aenean dignissim fames nisl sed pulvinar. Nullam ut tortor in ac semper enim cursus quisque. Amet felis at consectetur faucibus. At nisl dictum in eu quisque eget. Integer lacinia non mattis diam ornare. Tempor leo erat risus faucibus nulla pharetra egestas ultrices.
-                            </a>
-                        </div>
-                        <div className='commentOwner'>
-                            <img alt='' src={'assets/img/TepeHomeLittlePhoto.svg'}></img>
-                            <a className='commentOwnerFirstSpan'>Lorem ipsum</a>
-                            <a className='commentOwnerSecondSpan'>Tepe home</a>
-                            <img alt='' src={'assets/img/littleStar.svg'}></img>
-                        </div>
-                    </div>
-                    <div className='customerComment'>
-                        <div className='commentSymbol'>
-                            <img alt='' src={'assets/img/commentSymbol.svg'}></img>
-                        </div>
-                        <div className='commentSpan'>
-                            <a>Lorem ipsum dolor sit amet consectetur. Potenti turpis aenean tempus aliquam metus at. Ultricies vel adipiscing volutpat erat. Elit diam vulputate id ullamcorper in tellus vulputate donec. Faucibus quisque euismod sodales nunc. Velit mattis accumsan aenean dignissim fames nisl sed pulvinar. Nullam ut tortor in ac semper enim cursus quisque. Amet felis at consectetur faucibus. At nisl dictum in eu quisque eget. Integer lacinia non mattis diam ornare. Tempor leo erat risus faucibus nulla pharetra egestas ultrices.
-                            </a>
-                        </div>
-                        <div className='commentOwner'>
-                            <img alt='' src={'assets/img/TepeHomeLittlePhoto.svg'}></img>
-                            <a className='commentOwnerFirstSpan'>Lorem ipsum</a>
-                            <a className='commentOwnerSecondSpan'>Tepe home</a>
-                            <img alt='' src={'assets/img/littleStar.svg'}></img>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div className='thirdComment'>
-            <div className='commentHeader'>
-                    <a>People love Sugar</a>
-                </div>
-                <div className='comments'>
-                    <div className='customerComment'>
-                        <div className='commentSymbol'>
-                            <img alt='' src={'assets/img/commentSymbol.svg'}></img>
-                        </div>
-                        <div className='commentSpan'>
-                            <a>Lorem ipsum dolor sit amet consectetur. Potenti turpis aenean tempus aliquam metus at. Ultricies vel adipiscing volutpat erat. Elit diam vulputate id ullamcorper in tellus vulputate donec. Faucibus quisque euismod sodales nunc. Velit mattis accumsan aenean dignissim fames nisl sed pulvinar. Nullam ut tortor in ac semper enim cursus quisque. Amet felis at consectetur faucibus. At nisl dictum in eu quisque eget. Integer lacinia non mattis diam ornare. Tempor leo erat risus faucibus nulla pharetra egestas ultrices.
-                            </a>
-                        </div>
-                        <div className='commentOwner'>
-                            <img alt='' src={'assets/img/TepeHomeLittlePhoto.svg'}></img>
-                            <a className='commentOwnerFirstSpan'>Lorem ipsum</a>
-                            <a className='commentOwnerSecondSpan'>Tepe home</a>
-                            <img alt='' src={'assets/img/littleStar.svg'}></img>
-                        </div>
-                    </div>
-                    <div className='customerComment'>
-                        <div className='commentSymbol'>
-                            <img alt='' src={'assets/img/commentSymbol.svg'}></img>
-                        </div>
-                        <div className='commentSpan'>
-                            <a>Lorem ipsum dolor sit amet consectetur. Potenti turpis aenean tempus aliquam metus at. Ultricies vel adipiscing volutpat erat. Elit diam vulputate id ullamcorper in tellus vulputate donec. Faucibus quisque euismod sodales nunc. Velit mattis accumsan aenean dignissim fames nisl sed pulvinar. Nullam ut tortor in ac semper enim cursus quisque. Amet felis at consectetur faucibus. At nisl dictum in eu quisque eget. Integer lacinia non mattis diam ornare. Tempor leo erat risus faucibus nulla pharetra egestas ultrices.
-                            </a>
-                        </div>
-                        <div className='commentOwner'>
-                            <img alt='' src={'assets/img/TepeHomeLittlePhoto.svg'}></img>
-                            <a className='commentOwnerFirstSpan'>Lorem ipsum</a>
-                            <a className='commentOwnerSecondSpan'>Tepe home</a>
-                            <img alt='' src={'assets/img/littleStar.svg'}></img>
-                        </div>
-                    </div>
-                    <div className='customerComment'>
-                        <div className='commentSymbol'>
-                            <img alt='' src={'assets/img/commentSymbol.svg'}></img>
-                        </div>
-                        <div className='commentSpan'>
-                            <a>Lorem ipsum dolor sit amet consectetur. Potenti turpis aenean tempus aliquam metus at. Ultricies vel adipiscing volutpat erat. Elit diam vulputate id ullamcorper in tellus vulputate donec. Faucibus quisque euismod sodales nunc. Velit mattis accumsan aenean dignissim fames nisl sed pulvinar. Nullam ut tortor in ac semper enim cursus quisque. Amet felis at consectetur faucibus. At nisl dictum in eu quisque eget. Integer lacinia non mattis diam ornare. Tempor leo erat risus faucibus nulla pharetra egestas ultrices.
-                            </a>
-                        </div>
-                        <div className='commentOwner'>
-                            <img alt='' src={'assets/img/TepeHomeLittlePhoto.svg'}></img>
-                            <a className='commentOwnerFirstSpan'>Lorem ipsum</a>
-                            <a className='commentOwnerSecondSpan'>Tepe home</a>
-                            <img alt='' src={'assets/img/littleStar.svg'}></img>
-                        </div>
-                    </div>
-                </div>
-            </div>  */}
-            
-           <img className='commentsRightButton' alt='' src={'assets/img/commentsRightButton.svg'}></img>
-        </div>
+            <img className='commentsRightButton' alt='' src={'assets/img/commentsRightButton.svg'} onClick={() => { next(); }}></img>
+        </div >
     );
 }
