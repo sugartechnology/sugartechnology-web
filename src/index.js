ed0bcb27c6c2b3962e940398440ae1ac47acca4f
Abdurrahmant <aturkeri@sugartech.io>
2023-07-27 20:34:58 +0300

    GA entegrasyon

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import ReactGA from "react-ga4";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -13,7 +14,24 @@ root.render(
  
 );
 
+//Initialize GA4
+ReactGA.initialize("G-5JQQ5JMVE3");
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+
+  </React.StrictMode>
+);
+const SendAnalytics = ()=> {
+  ReactGA.send({
+    hitType: "pageview",
+    page: window.location.pathname,
+  });
+}
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals(SendAnalytics);
+
