54556a681548ec3ba7aa886dfa9d85a288539fd7
berkay <berkay.kaynar65@gmail.com>
2023-09-08 10:12:09 +0300

    main solution header font size ve vestel kullanıcı yorumu ana sayfa

--- src/components/CustomerComments/CustomerComments.js
+++ src/components/CustomerComments/CustomerComments.js
@@ -3,87 +3,95 @@ import { useTranslation } from "react-i18next";
 import React, { useState } from 'react';
 
 export const CustomerComments = (props) => {
-    const { t } = useTranslation();
-    const [index, setIndex] = useState(0);
-    const array = [
-      {
-        trustedByMillionsOfCompany: 'commentHeader',
-        commentSpan: 'commentSpan',
-        commentHeader: 'Genel Müdür Yardımcısı',
-        commentAuthor: 'Tuğba Karayel Özcan',
-        customerPhoto:'https://sugar.mncdn.com/sugartechwebsite/assets/img/sugar/tugba_karayel.jpeg',
-        companyLogo: 'assets/img/simurgLogo.svg'
-      },
-      {
-        trustedByMillionsOfCompany: 'commentHeader2',
-        commentSpan: 'commentSpan2',
-        commentHeader: 'Satış Direktörü',
-        commentAuthor: 'Yeşim Korkmaz',
-        customerPhoto:'https://sugar.mncdn.com/sugartechwebsite/assets/img/sugar/yesim_korkmaz.jpeg',
-        companyLogo: 'assets/img/tepe-home-min.svg'
-      },
-      {
-        trustedByMillionsOfCompany: 'commentHeader3',
-        commentSpan: 'commentSpan3',
-        commentHeader: 'İş Geliştirme ve Pazarlama Yöneticisi',
-        commentAuthor: 'Zeynep Yücel',
-        customerPhoto: 'assets/img/zeynepYucel.jpeg',
-        companyLogo: 'assets/img/ekohalilogo.svg'
-      },
-      {
-        trustedByMillionsOfCompany: 'commentHeader4',
-        commentSpan: 'commentSpan4',
-        commentHeader: 'İç Mimar ve Ürün Tasarımcısı',
-        commentAuthor: 'Serçil Tombak',
-        customerPhoto: 'assets/img/kys.webp',
-        companyLogo: 'assets/img/kys_design.svg'
-      },
-      {
-        trustedByMillionsOfCompany: 'commentHeader5',
-        commentSpan: 'commentSpan5',
-        commentHeader: 'Tasarımcı ve Şirket Yöneticisi',
-        commentAuthor: 'Göksal Günaydın',
-        customerPhoto: 'assets/img/kys2.jpeg',
-        companyLogo: 'assets/img/kys_design.svg'
-      },
-      {
-        trustedByMillionsOfCompany: 'commentHeader6',
-        commentSpan: 'commentSpan6',
-        commentHeader: 'Pazarlama Müdürü',
-        commentAuthor: 'Ceren Evcimen',
-        customerPhoto: 'assets/img/cerenEvcimen.jpeg',
-        companyLogo: 'assets/img/tepe-home-min.svg'
-      },
-    ];
+  const { t } = useTranslation();
+  const [index, setIndex] = useState(0);
+  const array = [
+    {
+      trustedByMillionsOfCompany: 'commentHeader',
+      commentSpan: 'commentSpan',
+      commentHeader: 'Genel Müdür Yardımcısı',
+      commentAuthor: 'Tuğba Karayel Özcan',
+      customerPhoto: 'https://sugar.mncdn.com/sugartechwebsite/assets/img/sugar/tugba_karayel.jpeg',
+      companyLogo: 'assets/img/simurgLogo.svg'
+    },
+    {
+      trustedByMillionsOfCompany: 'commentHeader2',
+      commentSpan: 'commentSpan2',
+      commentHeader: 'Satış Direktörü',
+      commentAuthor: 'Yeşim Korkmaz',
+      customerPhoto: 'https://sugar.mncdn.com/sugartechwebsite/assets/img/sugar/yesim_korkmaz.jpeg',
+      companyLogo: 'assets/img/tepe-home-min.svg'
+    },
+    {
+      trustedByMillionsOfCompany: 'commentHeader3',
+      commentSpan: 'commentSpan3',
+      commentHeader: 'İş Geliştirme ve Pazarlama Yöneticisi',
+      commentAuthor: 'Zeynep Yücel',
+      customerPhoto: 'assets/img/zeynepYucel.jpeg',
+      companyLogo: 'assets/img/ekohalilogo.svg'
+    },
+    {
+      trustedByMillionsOfCompany: 'commentHeader4',
+      commentSpan: 'commentSpan4',
+      commentHeader: 'İç Mimar ve Ürün Tasarımcısı',
+      commentAuthor: 'Serçil Tombak',
+      customerPhoto: 'assets/img/kys.webp',
+      companyLogo: 'assets/img/kys_design.svg'
+    },
+    {
+      trustedByMillionsOfCompany: 'commentHeader5',
+      commentSpan: 'commentSpan5',
+      commentHeader: 'Tasarımcı ve Şirket Yöneticisi',
+      commentAuthor: 'Göksal Günaydın',
+      customerPhoto: 'assets/img/kys2.jpeg',
+      companyLogo: 'assets/img/kys_design.svg'
+    },
+    {
+      trustedByMillionsOfCompany: 'commentHeader6',
+      commentSpan: 'commentSpan6',
+      commentHeader: 'Pazarlama Müdürü',
+      commentAuthor: 'Ceren Evcimen',
+      customerPhoto: 'assets/img/cerenEvcimen.jpeg',
+      companyLogo: 'assets/img/tepe-home-min.svg'
+    },
+    {
+      trustedByMillionsOfCompany: 'listenToPartnerHeader',
+      commentSpan: 'whiteGoodsPartnerSpan',
+      commentHeader: 'Marketing Communications&Retail Marketing Executive',
+      commentAuthor: 'Buket Kaplan',
+      customerPhoto: 'assets/img/buket_kaplan_vestel.jpeg',
+      companyLogo: './assets/img/vestel-logo-min.svg'
+    },
+  ];
 
-    const next = () => {
-        setIndex((index + 1) % array.length);
-    };
-  
-    const prev = () => {
-        setIndex(index - 1 < 0 ? index - 1 + array.length : index - 1);
-    };
-  
-    return (
-      <div className='customerCommentsContainer'>
-        <img className='commentsLeftButton' alt='' src={'assets/img/CommentsLeftButton.svg'} onClick={() => {prev();}}></img>
-          <img className='redBackgroundCircle1' alt='' src={'./assets/img/circle1.svg'}></img>
-          <img className='redBackgroundCircle2' alt='' src={'./assets/img/circle2.svg'}></img>
-          <img className='redBackgroundCircle3' alt='' src={'./assets/img/circle3.svg'}></img>
-          <div className="commentContainer">
-            <div className='commentSpans'>
-              <a className='firstCommentSpan'>{t(array[index].trustedByMillionsOfCompany)}</a>
-              <a className='secondCommentSpan'>“</a>
-              <a className='thirdCommentSpan'>{t(array[index].commentSpan)}</a>
-            </div>
-            <div className='commentCustomer'>
-              <img className='customerPhoto' alt='' src={t(array[index].customerPhoto)}></img>
-              <h1>{t(array[index].commentAuthor)}</h1>
-              <a>{array[index].commentHeader}</a>
-              <img className='companyLogo' alt='' src={array[index].companyLogo}></img>
-            </div>
-          </div>
-        <img className='commentsRightButton' alt='' src={'assets/img/CommentsRightButton.svg'} onClick={() => {next();}}></img>
-      </div>
-    );
+  const next = () => {
+    setIndex((index + 1) % array.length);
   };
+
+  const prev = () => {
+    setIndex(index - 1 < 0 ? index - 1 + array.length : index - 1);
+  };
+
+  return (
+    <div className='customerCommentsContainer'>
+      <img className='commentsLeftButton' alt='' src={'assets/img/CommentsLeftButton.svg'} onClick={() => { prev(); }}></img>
+      <img className='redBackgroundCircle1' alt='' src={'./assets/img/circle1.svg'}></img>
+      <img className='redBackgroundCircle2' alt='' src={'./assets/img/circle2.svg'}></img>
+      <img className='redBackgroundCircle3' alt='' src={'./assets/img/circle3.svg'}></img>
+      <div className="commentContainer">
+        <div className='commentSpans'>
+          <a className='firstCommentSpan'>{t(array[index].trustedByMillionsOfCompany)}</a>
+          <a className='secondCommentSpan'>“</a>
+          <a className='thirdCommentSpan'>{t(array[index].commentSpan)}</a>
+        </div>
+        <div className='commentCustomer'>
+          <img className='customerPhoto' alt='' src={t(array[index].customerPhoto)}></img>
+          <h1>{t(array[index].commentAuthor)}</h1>
+          <a>{array[index].commentHeader}</a>
+          <img className='companyLogo' alt='' src={array[index].companyLogo}></img>
+        </div>
+      </div>
+      <img className='commentsRightButton' alt='' src={'assets/img/CommentsRightButton.svg'} onClick={() => { next(); }}></img>
+    </div>
+  );
+};
--- src/pages/main/MainPage.js
+++ src/pages/main/MainPage.js
@@ -6,7 +6,7 @@ import React, { useState, useEffect } from 'react';
 import { MainSectoralSolution } from "../../components/MainSectoralSolution/MainSectoralSolution";
 import { Cookies } from "../../components/Cookies/Cookies";
 
-export const MainPage = ()=>{
+export const MainPage = () => {
     const [isTablet, setIsTablet] = useState(false);
     const [isMobile, setIsMobile] = useState(false);
 
@@ -14,7 +14,7 @@ export const MainPage = ()=>{
         const handleResize = () => {
             setIsMobile(window.innerWidth <= 700);
             setIsTablet(window.innerWidth > 700 && window.innerWidth <= 1100);
-            
+
         };
 
         handleResize();
@@ -22,22 +22,22 @@ export const MainPage = ()=>{
         window.addEventListener('resize', handleResize);
 
         return () => {
-        window.removeEventListener('resize', handleResize);
+            window.removeEventListener('resize', handleResize);
         };
     }, []);
     return (<div className={`${isMobile ? 'mobile' : ''} ${isTablet ? 'tablet' : ''}  MainPage`}>
         <Cookies></Cookies>
-         <MainPageSlider>
+        <MainPageSlider>
             <MainPageSliderItem src={process.env.PUBLIC_URL + '/home.svg'} ssrc={process.env.PUBLIC_URL + '/home_selected.svg'}></MainPageSliderItem>
             <MainPageSliderItem src={process.env.PUBLIC_URL + '/metaverse.svg'} ssrc={process.env.PUBLIC_URL + '/metaverse_selected.svg'}></MainPageSliderItem>
             <MainPageSliderItem src={process.env.PUBLIC_URL + '/cloth.svg'} ssrc={process.env.PUBLIC_URL + '/cloth_selected.svg'}></MainPageSliderItem>
             <MainPageSliderItem src={process.env.PUBLIC_URL + '/watch.svg'} ssrc={process.env.PUBLIC_URL + '/watch_selected.svg'}></MainPageSliderItem>
             <MainPageSliderItem src={process.env.PUBLIC_URL + '/shoe.svg'} ssrc={process.env.PUBLIC_URL + '/shoe_selected.svg'}></MainPageSliderItem>
             <MainPageSliderItem src={process.env.PUBLIC_URL + '/joint.svg'} ssrc={process.env.PUBLIC_URL + '/joint_selected.svg'}></MainPageSliderItem>
-          </MainPageSlider>
-          <About></About>
-          <Customers></Customers>
-          <MainSectoralSolution></MainSectoralSolution>
-          <CustomerComments></CustomerComments>
+        </MainPageSlider>
+        <About></About>
+        <Customers></Customers>
+        <MainSectoralSolution></MainSectoralSolution>
+        <CustomerComments></CustomerComments>
     </div>);
 }
